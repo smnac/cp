409dc619cc32f2eb48271fcdc6ee2dcd8cab8e7d
AdarshA <adarsha@AdarshAs-MacBook-Air.local>
2022-02-02 23:34:59 +0530

    Construct Binary Tree from inorder and postorder traversals

---
+++ tree/TreeConstruction.java
@@ -0,0 +1,34 @@
+package cp.tree;
+
+public class TreeConstruction {
+    private int pp; //postorder pointer
+     
+       
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        
+        
+        pp=inorder.length-1;
+        return constructTree(inorder,postorder,0,inorder.length-1);   
+        
+    }
+    
+    private TreeNode constructTree(int[] inorder,int[] postorder,int start,int end)
+    {
+        if(start>end || pp<0) 
+            return null;
+        TreeNode root = new TreeNode(postorder[pp--]);
+        int cur=findInorderPos(inorder,root.val);
+        root.right=constructTree(inorder,postorder,cur+1,end);
+        root.left=constructTree(inorder,postorder,start,cur-1);
+       
+        return root;
+    }
+    
+    private int findInorderPos(int[] inorder,int val)
+    {
+        for(int i=inorder.length-1;i>=0;i--)
+            if(inorder[i]==val)
+                return i;
+        return -1;
+    }
+}
