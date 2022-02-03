44da50a57664500baee8d215924a95160d619540
AdarshA <adarsha@AdarshAs-MacBook-Air.local>
2022-02-04 00:35:34 +0530

    Add comments to Tree Next pointer code

--- tree/TreeNextPointer.java
+++ tree/TreeNextPointer.java
@@ -1,5 +1,13 @@
 package cp.tree;
 
+/*
+Populate each next pointer to point to its next right node. 
+If there is no next right node, the next pointer should be 
+set to NULL.
+
+Initially, all next pointers are set to NULL.
+*/
+
 class Node {
     int val;
     Node left;
