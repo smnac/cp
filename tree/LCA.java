69f69f6bc6abc6ed6c282d8e3bcfe15acd84528a
AdarshA <adarsha@AdarshAs-MacBook-Air.local>
2022-03-07 23:00:24 +0530

    Add Tree Program

---
+++ tree/LCA.java
@@ -0,0 +1,16 @@
+
+public class LCA {
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+		if (root.val < p.val && root.val < q.val)
+			return lowestCommonAncestor(root.right, p, q);
+		
+		else if (root.val > p.val && root.val > q.val)
+			return lowestCommonAncestor(root.left, p, q);
+		
+		else
+			return root;
+	}
+
+}
