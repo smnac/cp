c340d7041a9c8b78dd4ef9065a9c71199937fdb3
AdarshA <adarsha@AdarshAs-MacBook-Air.local>
2022-03-08 23:59:52 +0530

    Add Tree Program

---
+++ tree/BalancedTree.java
@@ -0,0 +1,19 @@
+
+public class BalancedTree {
+
+	public boolean isBalanced(TreeNode root) {
+
+		if (root == null)
+			return true;
+
+		return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+	}
+
+	int height(TreeNode root) {
+		if (root == null)
+			return -1;
+
+		return 1 + Math.max(height(root.left), height(root.right));
+	}
+
+}
