e5274c688cf37aa5846c844ffc3262644a719d23
smnac <66745952+smnac@users.noreply.github.com>
2022-05-06 22:57:56 +0530

    Update RemoveDuplicateString.java

--- stack/RemoveDuplicateString.java
+++ stack/RemoveDuplicateString.java
@@ -78,4 +78,25 @@ class RemoveDuplicateString {
         return res.reverse().toString();
         
     }
+ 
+ public String removeDuplicatesRecursion(String s, int k) {
+        int count=1;
+        
+        for(int i=1;i<s.length();i++)
+        {
+            if(s.charAt(i)==s.charAt(i-1))
+                count++;
+            else
+                count=1;
+            
+            if(count==k)
+            {
+                return removeDuplicates(s.substring(0,i-k+1)+s.substring(i+1),k);
+            }
+        }
+        
+        return s;
+    
+        
+    }
 }
