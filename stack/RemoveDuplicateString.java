7ebc22809d65eb9039963b16e237e6108d7b9028
smnac <66745952+smnac@users.noreply.github.com>
2022-05-06 22:50:10 +0530

    Create RemoveDuplicateString.java

---
+++ stack/RemoveDuplicateString.java
@@ -0,0 +1,81 @@
+/*
+
+1209. Remove All Adjacent Duplicates in String II
+Medium
+
+3509
+
+67
+
+Add to List
+
+Share
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+2 <= k <= 104
+s only contains lower case English letters.
+
+*/
+
+class RemoveDuplicateString {
+    public String removeDuplicates(String s, int k) {
+        
+    Stack<int[]> stack = new Stack<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            
+            if(!stack.isEmpty() && stack.peek()[0]==ch)
+            {
+                stack.peek()[1]++;
+            }
+            else
+            {
+                stack.push(new int[]{ch,1});
+            }
+            
+            if(stack.peek()[1]==k)
+                stack.pop();
+        }
+        
+        StringBuilder res=new StringBuilder();
+        
+        while(!stack.isEmpty())
+        { int[] cc = stack.pop();
+            for(int i=0;i<cc[1];i++)
+            res.append((char)cc[0]);
+        }
+        
+        return res.reverse().toString();
+        
+    }
+}
