f82c0349c9b63464dfbd6c73ced9b37a341ee268
smnac <66745952+smnac@users.noreply.github.com>
2022-09-07 08:39:10 +0900

    Create 008InsertInterval.java

---
+++ grind169/008InsertInterval.java
@@ -0,0 +1,60 @@
+/*
+  57. Insert Interval
+  https://leetcode.com/problems/insert-interval/
+  
+  You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 105
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 105
+*/
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        LinkedList<int[]> res = new LinkedList<>();
+        
+        for(int i=0;i<intervals.length;i++){
+            if(newInterval[1]<intervals[i][0]){ // this is going to be the first interval in the intervals.
+                res.add(newInterval);
+                
+                // insert remaining intervals into the res array.
+                for(int j=i;j<intervals.length;j++){
+                    res.add(intervals[j]);
+                }
+                return res.toArray(new int[res.size()][2]);
+            }
+            
+            else if(newInterval[0]>intervals[i][1]){ // add the new interval to the end.
+                res.add(intervals[i]);
+            } else { // this is last condition where we need to insert the interval in the middle.
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
+        res.add(newInterval); // should be outside
+
+        return res.toArray(new int[res.size()][2]);
+    }
+}
