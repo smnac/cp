dbd236d449345dad4d82a51a04dd45a74e7fbfb6
smnac <66745952+smnac@users.noreply.github.com>
2022-09-09 10:52:38 +0900

    Update 009ThreeSum.java

--- grind169/009ThreeSum.java
+++ grind169/009ThreeSum.java
@@ -136,3 +136,68 @@ class Solution {
         return res;
     }
 }
+
+
+/*
+
+16. 3Sum Closest
+
+https://leetcode.com/problems/3sum-closest/
+
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+ 
+
+Constraints:
+
+3 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+
+*/
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int diff=Integer.MAX_VALUE;
+       for(int i=0;i<nums.length-2;i++)
+       {
+           int low=i+1;
+           int high=nums.length-1;
+           while(low<high)
+           {
+               int sum=nums[i]+nums[low]+nums[high];
+               if(Math.abs(target-sum)<Math.abs(diff)) // diff comp is abs
+                   diff=target-sum;   //need to store actual diff
+                 if(diff==0)
+                     break;
+               
+               if(sum<target)
+                   low++;
+               else
+                   high--;
+           }
+           
+          if(diff==0)
+              break;
+       }
+        
+         return target-diff;
+        
+    }
+}
