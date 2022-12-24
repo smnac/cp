f2cd237713601c293fe5a127cf50b815b6083932
smnac <66745952+smnac@users.noreply.github.com>
2022-12-24 20:54:23 +0530

    Update 000SetMatrixZero.java

--- stsheet/000SetMatrixZero.java
+++ stsheet/000SetMatrixZero.java
@@ -1,4 +1,5 @@
 /* 73. Set Matrix Zeroes
+https://leetcode.com/problems/set-matrix-zeroes/
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
 You must do it in place.
