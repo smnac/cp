a991ea5aadf229d6094a83c68e7e7067647b1bc5
smnac <66745952+smnac@users.noreply.github.com>
2022-12-25 12:39:25 +0530

    Create 009FindDuplicateNumber.java

---
+++ stsheet/009FindDuplicateNumber.java
@@ -0,0 +1,71 @@
+/*
+287. Find the Duplicate Number
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and uses only constant extra space.
+ 
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+ 
+Constraints:
+1 <= n <= 105
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ 
+Follow up:
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?
+*/
+
+// negative marking and restore
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int duplicate = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = Math.abs(nums[i]);
+            if (nums[cur] < 0) {
+                duplicate = cur;
+                break;
+            }
+            nums[cur] *= -1;
+        }
+        
+        // Restore numbers
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = Math.abs(nums[i]);
+
+        return duplicate;
+    }
+}
+
+
+// Flyod cycle detection
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        int fast = nums[0];
+        int slow = nums[0];
+        
+        do
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        } while(fast!=slow);
+        
+        fast=nums[0];
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        
+        return slow;
+        
+    }
+}
