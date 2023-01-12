c69a7b6b4f878a9aedfa2705ac935ab326bca6d2
smnac <66745952+smnac@users.noreply.github.com>
2023-01-12 08:45:59 +0530

    Create RobotRoomCleaner.java

---
+++ recursion/RobotRoomCleaner.java
@@ -0,0 +1,67 @@
+/*
+489. Robot Room Cleaner
+https://leetcode.com/problems/robot-room-cleaner/
+ou are controlling a robot that is located somewhere in a room. The room is modeled as an m x n binary grid where 0 represents a wall and 1 represents an empty slot.
+
+The robot starts at an unknown location in the room that is guaranteed to be empty, and you do not have access to the grid, but you can move the robot using the given API Robot.
+
+You are tasked to use the robot to clean the entire room (i.e., clean every empty cell in the room). The robot with the four given APIs can move forward, turn left, or turn right. Each turn is 90 degrees.
+
+When the robot tries to move into a wall cell, its bumper sensor detects the obstacle, and it stays on the current cell.
+
+Design an algorithm to clean the entire room using the following APIs:
+
+interface Robot {
+  // returns true if next cell is open and robot moves into the cell.
+  // returns false if next cell is obstacle and robot stays on the current cell.
+  boolean move();
+
+  // Robot will stay on the same cell after calling turnLeft/turnRight.
+  // Each turn will be 90 degrees.
+  void turnLeft();
+  void turnRight();
+
+  // Clean the current cell.
+  void clean();
+}
+Note that the initial direction of the robot will be facing up. You can assume all four edges of the grid are all surrounded by a wall.
+
+*/
+
+class Solution {
+    // going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
+    int[][] directions = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+    Set<Pair<Integer, Integer>> visited = new HashSet();
+    Robot robot;
+
+    public void goBack() {
+        robot.turnRight();
+        robot.turnRight();
+        robot.move();
+        robot.turnRight();
+        robot.turnRight();
+    }
+
+    public void backtrack(int row, int col, int d) {
+        visited.add(new Pair(row, col));
+        robot.clean();
+        // going clockwise : 0: 'up', 1: 'right', 2: 'down', 3: 'left'
+        for (int i = 0; i < 4; ++i) {
+            int newD = (d + i) % 4;
+            int newRow = row + directions[newD][0];
+            int newCol = col + directions[newD][1];
+
+            if (!visited.contains(new Pair(newRow, newCol)) && robot.move()) {
+                backtrack(newRow, newCol, newD);
+                goBack();
+            }
+            // turn the robot following chosen direction : clockwise
+            robot.turnRight();
+        }
+    }
+
+    public void cleanRoom(Robot robot) {
+        this.robot = robot;
+        backtrack(0, 0, 0);
+    }
+}
