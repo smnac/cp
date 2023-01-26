473754199a6ca64313742bf3718824f1c88af31e
smnac <66745952+smnac@users.noreply.github.com>
2023-01-26 19:27:03 +0530

    Create 026MergeTwoSortedList.java

---
+++ stsheet/026MergeTwoSortedList.java
@@ -0,0 +1,37 @@
+/*
+https://leetcode.com/problems/merge-two-sorted-lists/
+*/
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        
+        while(list1!=null && list2!=null)
+        {
+            if(list1.val<list2.val)
+            {
+                cur.next=list1;
+                list1=list1.next;
+            }
+            else
+            {
+                cur.next=list2;
+                list2=list2.next;
+            }
+            cur=cur.next;
+                
+        }
+        
+        if(list1!=null)
+            cur.next=list1;
+        if(list2!=null)
+            cur.next=list2;
+        return head.next;
+        
+    }
+}
