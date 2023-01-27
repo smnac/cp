88746b83ef00a393b31a99619dcb1249bd944a51
smnac <66745952+smnac@users.noreply.github.com>
2023-01-27 11:06:36 +0530

    Update 048CombinationSum.java

--- stsheet/048CombinationSum.java
+++ stsheet/048CombinationSum.java
@@ -4,43 +4,36 @@ https://leetcode.com/problems/combination-sum/
 
 */
 
-import java.io.*;
-import java.util.*;
 class Solution {
-
-    private void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
-        if (ind == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList < > (ds));
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(candidates,0,candidates.length,target,new ArrayList<Integer>(),ans);
+        return ans;
+        
+    }
+    
+    private void helper(int[] arr,int index,int N,int target,List<Integer> cur,List<List<Integer>> ans)
+    {
+        
+        if(index==N)
+        {
+            if(target==0)
+            {
+                ans.add(new ArrayList<>(cur));
             }
             return;
         }
-
-        if (arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
-            ds.remove(ds.size() - 1);
+        
+        if(arr[index]<=target)
+        {
+            cur.add(arr[index]);
+            helper(arr,index,N,target-arr[index],cur,ans); // remember not index+1, again index infinte supply
+            cur.remove(cur.size()-1);
         }
-        findCombinations(ind + 1, arr, target, ans, ds);
+        helper(arr,index+1,N,target,cur,ans);
+        
     }
-    public List < List < Integer >> combinationSum(int[] candidates, int target) {
-        List < List < Integer >> ans = new ArrayList < > ();
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
-        return ans;
-    }
-}
-public class Main {
-    public static void main(String[] args) {
-        int arr[] = {2,3,6,7};
-        int target = 7;
-        Solution sol = new Solution();
-        List < List < Integer >> ls = sol.combinationSum(arr, target);
-        System.out.println("Combinations are: ");
-        for (int i = 0; i < ls.size(); i++) {
-            for (int j = 0; j < ls.get(i).size(); j++) {
-                System.out.print(ls.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
-    }
+    
+  
 }
