56d16fe7d84e4412e2b5d135f131a560b6e821aa
smnac <66745952+smnac@users.noreply.github.com>
2023-01-28 19:01:25 +0530

    Create 064AggressiveCows.java

---
+++ stsheet/064AggressiveCows.java
@@ -0,0 +1,14 @@
+/*
+Similar questions on leetcode
+1552. Magnetic Force Between Two Balls
+1870. Minimum Speed to Arrive on Time
+875.  Koko Eating Bananas
+1011. Capacity To Ship Packages Within D Days
+1283. Find the Smallest Divisor Given a Threshold
+1482. Minimum Number of Days to Make m Bouquets
+2064. Minimized Maximum of Products Distributed to Any Store
+1231. Divide Chocolate
+774.  Minimize Max Distance to Gas Station
+410.  Split Array Largest Sum
+
+*/
