09b659f6a5eeb7adaaaa304baa82ba885fc87a91
smnac <66745952+smnac@users.noreply.github.com>
2023-02-05 10:21:28 +0530

    Create 084ReverseWordsInString.java

---
+++ stsheet/084ReverseWordsInString.java
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/reverse-words-in-a-string/
+151. Reverse Words in a String
+*/
+
+public class Solution {
+    public String reverseWords(String s) {
+        String [] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        int end = words.length - 1;
+        for(int i = 0; i<= end; i++){
+            if(!words[i].isEmpty()) {
+                sb.insert(0, words[i]);
+                if(i < end) sb.insert(0, " ");
+            }
+        }
+        return sb.toString();
+    }
+}
