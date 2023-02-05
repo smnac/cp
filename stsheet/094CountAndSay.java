3b0c618e6a3c53078fd0f51c1ab4499de253d0e3
smnac <66745952+smnac@users.noreply.github.com>
2023-02-05 16:28:18 +0530

    Update 094CountAndSay.java

--- stsheet/094CountAndSay.java
+++ stsheet/094CountAndSay.java
@@ -2,3 +2,39 @@
 38. Count and Say
 https://leetcode.com/problems/count-and-say/
 */
+class Solution {
+    public String countAndSay(int n) {
+        
+        if(n==1)
+            return "1";
+        if(n==2)
+            return "11";
+        String str="11";
+       
+for(int i=3;i<=n;i++)
+{
+    int count=1;
+    int len =str.length();
+    StringBuilder temp=new StringBuilder();
+    for(int j=1;j<len;j++)
+    {
+        count=1;
+        while(j<len && str.charAt(j-1)==str.charAt(j)){
+            count++;
+            j++;
+        }
+        temp.append(count);
+        temp.append(str.charAt(j-1));
+    }
+    if(str.charAt(len-2)!=str.charAt(len-1)){
+        temp.append(""+1);
+        temp.append(str.charAt(len-1));
+    }
+    str=temp.toString();
+                
+    }
+
+        return str;
+        
+    }
+}
