f63b798f12d0a1bcdb291e814da01ef09940a6d6
smnac <66745952+smnac@users.noreply.github.com>
2023-02-05 17:19:21 +0530

    Update 098BTPostOrderTraversal.java

--- stsheet/098BTPostOrderTraversal.java
+++ stsheet/098BTPostOrderTraversal.java
@@ -22,3 +22,31 @@ class Solution {
         po.add(node.val);
     }
 }
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+                List<Integer> postorder = new ArrayList<>();
+        if(root==null)
+            return postorder;
+            
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        
+        stack1.push(root);
+        while(!stack1.isEmpty())
+        {
+            TreeNode cur=stack1.pop();
+            stack2.push(cur);
+            if(cur.left!=null)
+                stack1.push(cur.left);
+            if(cur.right!=null)
+                stack1.push(cur.right);
+        }
+
+        while(!stack2.isEmpty())
+            postorder.add(stack2.pop().val);
+        return postorder;
+        
+        
+    }
+}
