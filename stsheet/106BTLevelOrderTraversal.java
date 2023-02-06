061002a73ce4b89d62eb024253af35baa7941e17
smnac <66745952+smnac@users.noreply.github.com>
2023-02-06 08:12:37 +0530

    Update 106BTLevelOrderTraversal.java

--- stsheet/106BTLevelOrderTraversal.java
+++ stsheet/106BTLevelOrderTraversal.java
@@ -36,3 +36,43 @@ class Solution {
 }
     
 }
+
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> res = new ArrayList<>();
+        
+        if(root == null) return res;
+        boolean is_left=true;
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            LinkedList<Integer> ans = new LinkedList<>();
+            while(size>0){
+                size--;
+            TreeNode cur = queue.poll();
+                if(is_left)
+                ans.addLast(cur.val); //imp, add to tail
+                else
+                ans.addFirst(cur.val); //imp,add to head
+                
+            if(cur.left != null) 
+                queue.offer(cur.left);
+                
+            if(cur.right != null) 
+                queue.offer(cur.right);
+            
+               
+                
+        }
+            is_left=!is_left; //toggle direction
+             res.add(ans);
+        
+        
+    }
+        return res;
+        
+    }
+}
