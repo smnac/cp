da2a5180ffe8fd82af3818e5257a7bb5761c6d6d
smnac <66745952+smnac@users.noreply.github.com>
2023-02-06 09:39:43 +0530

    Update 116BTFlattenTree.java

--- stsheet/116BTFlattenTree.java
+++ stsheet/116BTFlattenTree.java
@@ -18,3 +18,24 @@ class Solution {
         
     }
 }
+
+//Iteration
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode cur=root;
+        TreeNode prev=null;
+        while(cur!=null)
+        {
+            if(cur.left!=null)
+            {
+                prev=cur.left;
+                while(prev.right!=null)
+                    prev=prev.right;
+                prev.right=cur.right;
+                cur.right=cur.left;
+                cur.left=null;
+            }
+            cur=cur.right;
+        }
+        
+    }
