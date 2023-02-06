a5d68e4f48ac6897589d7a9c5d9fba3121a26f00
smnac <66745952+smnac@users.noreply.github.com>
2023-02-06 09:30:55 +0530

    Create 116BTFlattenTree.java

---
+++ stsheet/116BTFlattenTree.java
@@ -0,0 +1,20 @@
+/*
+
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+114. Flatten Binary Tree to Linked List
+
+*/
+
+class Solution {
+    TreeNode prev=null;
+    public void flatten(TreeNode root) {
+        if(root==null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right=prev;
+        root.left=null;
+        prev=root;
+        
+    }
+}
